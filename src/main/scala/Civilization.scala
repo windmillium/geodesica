0b618cc477f70a8c9cc8f35ca7782afe74c098f3
Luke Sheridan <luke@windmillium.com>
2011-12-31 15:30:18 -0800

    Add zones to civilization.

--- src/main/scala/Civilization.scala
+++ src/main/scala/Civilization.scala
@@ -6,4 +6,6 @@ class Civilization(val name:String) {
   val queue = new JobQueue(name)
   val recipes = new ListBuffer[Recipe]
   var home: Block = _
+  val zones = new ListBuffer[Zone]
+trait Zone
 }
