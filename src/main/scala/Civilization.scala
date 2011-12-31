5150c0af5d921a5ec5569356f27716353c6f29f3
Luke Sheridan <luke@windmillium.com>
2011-12-31 15:30:54 -0800

    Add stockpile zone.

--- src/main/scala/Civilization.scala
+++ src/main/scala/Civilization.scala
@@ -7,5 +7,21 @@ class Civilization(val name:String) {
   val recipes = new ListBuffer[Recipe]
   var home: Block = _
   val zones = new ListBuffer[Zone]
+  val objects = new ListBuffer[Object]
+
+  def stockpiles = zones.collect({ case z:Stockpile => z})
+  def stockpileCapacity:Int = {
+    stockpiles.foldLeft(0)(_ + _.capacity)
+  }
+}
+
 trait Zone
+
+class Stockpile extends Zone {
+  import ObjectTemplate._
+  val blocks = new ListBuffer[Block]
+  def capacity = blocks.foldLeft(0)(_ + _.objectCapacity)
+  def finished = !blocks.exists(b => b.installedObject == null)
+
+  def requirements = blocks.map(b => (b,objectTemplate("Pallet")))
 }
