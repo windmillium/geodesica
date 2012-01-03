ca855a41651ebf97368c442f2045477f47201e69
Luke Sheridan <luke@windmillium.com>
2012-01-02 23:48:49 -0800

    Add SpaceFinder class
    Which looks inside a collection of blocks for a large enough free space.

---
+++ src/main/scala/SpaceFinder.scala
@@ -0,0 +1,5 @@
+package net.geodesica
+
+class SpaceFinder(blocks:Set[Block], desiredSize:Int) {
+  val location:Option[Block] = blocks.find(b => b.nearbyBlocks(desiredSize).forall({case(c,nb) => nb.installedObject == null && nb.zone == null}))
+}
