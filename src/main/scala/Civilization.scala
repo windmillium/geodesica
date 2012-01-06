712d8a0066ac9287e12d99e0a2af27ad509be415
Luke Sheridan <luke@windmillium.com>
2012-01-05 23:24:48 -0800

    Add free block method to farm.

--- src/main/scala/Civilization.scala
+++ src/main/scala/Civilization.scala
@@ -123,6 +123,7 @@ class Farm extends Zone {
   def requirements = Field.requirements(blocks)
 
   def finishedPlants = blocks.filter(b => b.plant != null && b.plant.crop > 0).map(b => b.plant)
+  def freeBlock = blocks.find(b => b.plant == null)
 }
 
 class BlockSet extends HashSet[Block] {
