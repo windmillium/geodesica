d1ba0faf0a8d1f5fb03df008d860ab544bcde050
Luke Sheridan <luke@windmillium.com>
2012-01-09 10:32:27 -0800

    SpaceFinder creates a 1 tile space around zones.

--- src/main/scala/SpaceFinder.scala
+++ src/main/scala/SpaceFinder.scala
@@ -1,5 +1,6 @@
 package net.geodesica
 
 class SpaceFinder(blocks:Set[Block], desiredSize:Int) {
-  val location:Option[Block] = blocks.find(b => b.nearbyBlocks(desiredSize).flatten.forall(nb => nb.canBuild))
+  val location:Option[Block] = blocks.find(b => b.nearbyBlocks(desiredSize+2).flatten.forall(nb => nb.canBuild)).flatMap(b =>
+      b.blockAt(b.coord+(1,1,0)))
 }
